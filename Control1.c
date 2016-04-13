7fcee6995e1d96304e5c7ae0bc57ef45cc660c65
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-04-13 20:43:09 -0300

    Modifications

--- Control1.c
+++ Control1.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void printBinaryChar(unsigned char c){
+	int i = 0;
+	printf("%i: ",c);
+	for(i=0; i<8; i++){
+		printf("%i",(c&(1<<(7-i)))>>(7-i));
+	}
+	printf("\n");
+}
+
+void printBinary(unsigned int b){
+	int i = 0;
+	for(i=0; i<32; i++){
+		printf("%i",b<<(31-i));
+	}
+	printf("\n");
+}
+
 int minus(int x){
 	return 1+~x;
 }
@@ -16,53 +33,43 @@ int abs(int x){
 }
 
 unsigned char rotateLeft(unsigned char c){
-	int mask = 1<<7;
-	int msb = c&mask;
-	return c<<1+msb;
+	unsigned char mask = 1<<7;
+	unsigned char msb = c&mask;
+	printf("LFT: ");
+	printBinaryChar((c<<1)+msb);
+	return (c<<1)+msb;
 }
 
 unsigned char rotateRight(unsigned char c){
-	int mask = 1;
-	int lsb = c&mask;
-	return c>>1+lsb<<7;
+	unsigned char mask = 1;
+	unsigned char lsb = c&mask;
+	printf("RGT: ");
+	printBinaryChar((c>>1)+(lsb<<7));
+	return (c>>1)+(lsb<<7);
 }
 
-unsigned char rotate(unsigned char c, int bit){
-	if(bits>0)
-		return rotateRight)(c);
+unsigned char rotate(unsigned char c, int bits){
+	printf("SRC: ");
+	printBinaryChar(c);
+	if(bits>=0)
+		return rotateRight(c);
 	else
 		return rotateLeft(c);
 }
 
-void printBinary(unsigned int b){
-	int i = 0;
-	for(i=0; i<32; i++){
-		printf("%i",b<<(31-i));
-	}
-	printf("\n");
-}
-
-void printBinaryChar(unsigned char c){
-	int i = 0;
-	for(i=0; i<8; i++){
-		printf("%i",b<<(7-i));
-	}
-	printf("\n");
-}
-
 int main()
 {
+	rotate(1,0);	
+	rotate(2,0);	
+	rotate(3,0);	
+	rotate(11,0);	
+	printf("\n");
 	
-
-	printBinaryChar(rotate(1),0);	
-	printBinaryChar(rotate(2),0);	
-	printBinaryChar(rotate(3),0);	
-	printBinaryChar(rotate(11),0);	
-	
-	printBinaryChar(rotate(1),-1);	
-	printBinaryChar(rotate(2),-1);	
-	printBinaryChar(rotate(3),-1);	
-	printBinaryChar(rotate(11),-1);	
+	rotate(1,-1);	
+	rotate(2,-1);	
+	rotate(3,-1);	
+	rotate(11,-1);	
+	printf("\n");
 	
 	printf("%i \n",abs(-1));
 	printf("%i \n",abs(-20));
