4fb7aadea0920582cb5afc9882a4a6c62fe4b6c7
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-04-13 20:23:20 -0300

    Modifications

--- Control1.c
+++ Control1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int minus(int x){
 	return 1+~x;
 }
@@ -14,10 +15,58 @@ int abs(int x){
 		return x;
 }
 
+unsigned char rotateLeft(unsigned char c){
+	int mask = 1<<7;
+	int msb = c&mask;
+	return c<<1+msb;
+}
+
+unsigned char rotateRight(unsigned char c){
+	int mask = 1;
+	int lsb = c&mask;
+	return c>>1+lsb<<7;
+}
+
+unsigned char rotate(unsigned char c, int bit){
+	if(bits>0)
+		return rotateRight)(c);
+	else
+		return rotateLeft(c);
+}
+
+void printBinary(unsigned int b){
+	int i = 0;
+	for(i=0; i<32; i++){
+		printf("%i",b<<(31-i));
+	}
+	printf("\n");
+}
+
+void printBinaryChar(unsigned char c){
+	int i = 0;
+	for(i=0; i<8; i++){
+		printf("%i",b<<(7-i));
+	}
+	printf("\n");
+}
 
 int main()
 {
+	
+
+	printBinaryChar(rotate(1),0);	
+	printBinaryChar(rotate(2),0);	
+	printBinaryChar(rotate(3),0);	
+	printBinaryChar(rotate(11),0);	
+	
+	printBinaryChar(rotate(1),-1);	
+	printBinaryChar(rotate(2),-1);	
+	printBinaryChar(rotate(3),-1);	
+	printBinaryChar(rotate(11),-1);	
+	
 	printf("%i \n",abs(-1));
 	printf("%i \n",abs(-20));
 	printf("%i \n",abs(20));
+	printf("%i \n",abs(0));
+	printf("%i \n",abs(-0));
 }
