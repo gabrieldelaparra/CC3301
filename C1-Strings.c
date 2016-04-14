cffe7fbfd13f711a9c0f3255a4488e7b553fb9d5
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-04-14 19:54:54 -0300

    Modificaciones Strings

--- C1-Strings.c
+++ C1-Strings.c
@@ -4,11 +4,16 @@
 void basic(){
 	char *s = "hola";
 	char r[] = {'h','o','l','a',0};
+	//char r[] = {'h','o','l','a'}; //r: hola+á@
 	char t[5] = {0};
 	
 	printf("s: %s \n",s);
 	printf("r: %s \n",r);
 	printf("t: %s \n",t);
+	
+	//s[0] = 'H'; //SegFault, cierra el programa;
+	r[0] = 'H';
+	printf("r[0] = H: %s \n",r);
 }
 
 int main(){
