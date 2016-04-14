384dd0cfe3d5872be051fc04a9c3160a195981e3
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-04-14 13:12:55 -0300

    Modificaciones C1-Punteros

--- C1-Puntero.c
+++ C1-Puntero.c
@@ -1,15 +1,67 @@
 #include <stdio.h>
 
-int v = 20;
+int y = 20;
 
 int noReturn(){
 	
 }
 
+void desreferencia(){
+	int v = 20;
+	int w;
+	int *p;
+	
+	p=&v;
+	printf("assigned p=&v: %i \n",p); 
+	printf("unassigned w: %i \n",w); 
+	w=*p;
+	printf("assigned w=*p: %i \n",w); 
+	*p=30;
+	printf("assigned *p=30: %i \n",*p); 
+	printf(" p: %i \n",p);
+	printf("&v: %i \n",&v);
+	printf("unassigned w: %i \n",w);
+}
+
 int main()
 {
-	v=10; //probando si se le puede asignar algo a una variable global. Si se puede.
-	printf("v: %i \n",v);
+	desreferencia();
+	// printf("y: %i \n",y); 
+	// y=10; //Probando si se le puede asignar algo a una variable global. Si se puede.
+	// printf("y: %i \n",y);
+	// printf("\n");
 	
-	printf("noReturn(): %i", noReturn()); //regresa 7;
+	// printf("noReturn(): %i \n", noReturn()); //regresa 7; Cambio a 24;
+	// printf("\n");
+	
+	// int *p;
+	// int a;
+
+	// printf("unassigned p: %i \n",p); //es la dirección de p;
+	// printf("unassigned *p: %i \n",*p); //es el valor al que apunta p;
+	// printf("unassigned &p: %i \n",&p); //es la dirección del puntero *p
+	// printf("\n");
+	
+	// printf("unassigned a: %i \n",a);
+	// printf("//unnasigned *a: //error: invalid type argument of unary '*' (have 'int')\n"); //error: invalid type argument of unary '*' (have 'int')
+	// printf("unassigned &a: %i \n",&a);
+	// printf("\n");
+	
+	
+	// a = 30;
+	// printf("assigned a: %i \n",a);
+	// printf("//unnasigned *a: //error: invalid type argument of unary '*' (have 'int')\n"); //error: invalid type argument of unary '*' (have 'int')
+	// printf("assigned &a: %i \n",&a); //&a es la dirección de p;
+	// printf("\n");
+	
+	// p = (int*)10;
+	// printf("assigned p=(int*)10: %i \n",p); //es la dirección de p;
+	// printf("//assigned *p:  Error, se cerro la aplicacion, probablemente por intentar leer memoria fuera de alcance;\n"); //es el valor al que apunta p; //Error, se cerró la aplicación, probablemente por intentar leer memoria fuera de alcance;
+	// printf("assigned &p: %i \n",&p); //es la dirección del puntero *p; se mantujo igual;
+	// printf("\n");
+	
+	// p=&a;
+	// printf("assigned p=&a: %i \n",p); //es la dirección de p;
+	// printf("assigned *p: %i \n",*p); //es el valor al que apunta p, en este caso, el valor de a;
+	// printf("assigned &p: %i \n",&p); //es la dirección del puntero *p; se mantujo igual de nuevo. En general &p no cambia, es la dirección del int* p;
 }
