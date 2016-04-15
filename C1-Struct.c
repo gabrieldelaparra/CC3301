70bd0406098351e02f40852c1ac51e80f8a0bbf5
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-04-15 01:54:56 -0300

    Modificaciones Struct

--- C1-Struct.c
+++ C1-Struct.c
@@ -2,18 +2,114 @@
 #include <strings.h> //strings
 #include <stdlib.h> //malloc
 
+typedef struct node{
+	int value;
+	struct node *next;
+} Node;
+
+void printNode(Node* n){
+	printf("Node: Address: %i Value: %i Next: %d\n", &n, n->value, n->next==NULL ? 0xFF : (n->next)->value);
+}
+
+void printNodeList(Node *n){
+	if(n != NULL){
+		printNode(n);
+		printNodeList(n->next);
+	}
+}
+
+void addNodeEnd(Node* head, Node* new)
+{
+	Node* current = head;
+	while(current->next != NULL){
+		current=current->next;
+	}
+	current->next=new;
+}
+
+Node* insertOrdered(Node* head, Node* new){
+	if(new->value<head->value){
+		printf("Insert Head\n");
+		new->next=head;
+		return new;
+	}
+	
+	Node* current = head;
+	
+	while(current!=NULL){
+		if(current->next==NULL){
+			printf("Insert End\n");
+			current->next = new;
+			return head;
+		}
+		
+		int nextValue = (current->next)->value;
+		
+		if(nextValue < new->value){
+			printf("Next\n");
+			current = current->next;
+		}
+		else{ //nextValue > new->value
+			printf("Insert: current:%i new: %i\n", current->value, new->value);
+			new->next=current->next;
+			current->next = new;
+			return head;
+		}
+	}
+}
+
+void insertNodes(){
+	Node n5 = {5,NULL};
+	Node n3 = {3,&n5};
+	Node n1 = {1,&n3};
+	Node* head = &n1;
+	printNodeList(head);
+	printf("\n");
+	
+	printf("AddEnd\n");
+	Node n7 = {7, NULL};
+	addNodeEnd(head, &n7);
+	printNodeList(head);
+	printf("\n");
+
+	printf("Insert 0:\n");
+	Node n0 = {0,NULL};	
+	head = insertOrdered(head,&n0);
+	printNodeList(head);
+	printf("\n");
+	
+	printf("Insert 4:\n");
+	Node n4 = {4,NULL};	
+	head = insertOrdered(head,&n4);
+	printNodeList(head);
+	printf("\n");
+	
+	printf("Insert 9:\n");
+	Node n9 = {9,NULL};	
+	head = insertOrdered(head,&n9);
+	printNodeList(head);
+	printf("\n");
+	
+	printf("Insert 2:\n");
+	Node n2 = {2,NULL};	
+	head = insertOrdered(head,&n2);
+	printNodeList(head);
+	printf("\n");
+}
 
 
 int main()
 {
-	struct punto w={1.0,-2.5};
+	insertNodes();
+
+	// struct punto w={1.0,-2.5};
 	// struct punto z={2.0,0};
-	struct punto q = NewPoint(2,0);
-	struct punto *r = mallocPoint(2,0);
+	// struct punto q = NewPoint(2,0);
+	// struct punto *r = mallocPoint(2,0);
 	
-	printPnt(SumaP(&w,&q));
-	printPnt(Suma(&w,r));
-	free(r);
+	// printPnt(SumaP(&w,&q));
+	// printPnt(Suma(&w,r));
+	// free(r);
 	// basic();
 }
 
