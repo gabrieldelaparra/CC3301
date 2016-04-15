8a06d473598b39ff10734e44be36dcf2dce143a6
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-04-14 23:54:04 -0300

    Modificaciones Struct

--- C1-Struct.c
+++ C1-Struct.c
@@ -1,4 +1,45 @@
+#include <stdio.h> //printf
+#include <strings.h> //strings
+#include <stdlib.h> //malloc
+
+struct punto{
+	float x;
+	float y;
+};
+
+void printPnt(struct punto p){
+	printf("p=(%f, %f)\n",p.x,p.y);	
+}
+
+struct punto Suma(struct punto *u, struct punto *v){
+	struct punto s={(*u).x+(*v).x,(*u).y+(*v).y};
+	return s;
+}
+
+struct punto SumaP(struct punto *u, struct punto *v){
+	struct punto s={u->x+v->x,u->y+v->y};
+	return s;
+}
+
+struct punto NewPoint(float x, float y){
+	struct punto n = {x,y};
+	return n;
+}
+
+void basic(){
+struct punto u, v;
+struct punto z={1.0,-2.5};
+
+z.x=2.0;
+printf("z=(%f, %f)\n",z.x,z.y);	
+}
+
 int main()
 {
-
+	struct punto w={1.0,-2.5};
+	// struct punto z={2.0,0};
+	struct punto q = NewPoint(2,0);
+	printPnt(SumaP(&w,&q));
+	printPnt(Suma(&w,&q));
+	// basic();
 }
