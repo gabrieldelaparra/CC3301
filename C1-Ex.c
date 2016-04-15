7a4fd5858132e558dcc1751854e3ebb389d9c18e
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-04-15 04:27:51 -0300

    Modificaciones examples

--- C1-Ex.c
+++ C1-Ex.c
@@ -3,14 +3,39 @@
 #include <stdlib.h>
 
 int espacio(char *s, char *p, char *r) {
-  return 0;
+  int ss = strlen(s);
+  int pp = strlen(p);
+  int rr = strlen(r);
+  int i= 0;
+  int matches = 0;
+  for(i=0;i<ss-pp;i++)
+  {
+	  if(strncmp(s+i,p,pp)==0)matches++;
+  }
+  return ss+(rr-pp)*matches+1;
 }
 
 char *substituir(char *s, char *p, char *r) {
-  return s;
+  int ss = strlen(s);
+  int pp = strlen(p);
+  int rr = strlen(r);
+  int esp = espacio(s,p,r);
+  char* sub = (char*)malloc(sizeof(char)*esp);
+  int i=0;
+  
+  for(i=0;i<ss-pp;i++){
+	  if(strncmp(s+i,p,pp)==0){
+		strcpy(sub+i,r);	  
+		i=i+rr;
+	  }
+	  else sub[i]=s[i];
+  }
+  sub[esp]=0;
+  return sub;
 }
 
 int main() {
+	// printf("%i",espacio("hola mundo", "hello", "bonjour"));
   if (espacio("hola mundo", "hello", "bonjour")!=11)
     printf("falla test 1\n");
   else if (espacio("hola mundo", "ola", "ello")!=12)
