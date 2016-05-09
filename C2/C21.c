622cde600e0546eda2edc820b95b3beba65cab80
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-05-09 19:00:33 -0300

    Changes

--- C2/C21.c
+++ C2/C21.c
@@ -1,3 +1,14 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+typedef struct{
+	char E;
+	pthread_mutex_t m;
+	pthread_cond_t cond;
+} Est;
+
+int main(){
+	
+}
