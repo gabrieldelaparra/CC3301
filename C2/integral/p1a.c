d303d18b55b6c738a553486cd7d0699966c97788
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-05-11 15:40:47 -0300

    Integral, P1b

--- C2/integral/p1a.c
+++ C2/integral/p1a.c
@@ -1,11 +1,12 @@
 #include "integral.h"
 typedef double (*Function) (void *p, double x);
+
 /* Programe aca la funcion integral */
 double integral(Function f, void *p, double xi, double xf, int n){
 	double h = (xf-xi)/n;
 	double a = (f(p,xi)+f(p,xf))/2;
 	double s = 0;
-	for(int i = 1; i<=n-1; i++){
+	for(int i = 1; i<n; i++){
 		s = s + f(p,xi+i*h);
 	}
 	return h*(a+s);
--- C2/integral/p1b.c
+++ C2/integral/p1b.c
@@ -1,15 +1,46 @@
 #include "integral.h"
-
+#include <stdlib.h>
 /* estas funciones aparecen en el enunciado y las necesitara */
 
 double g_aux(void *ptr, double x) {
- double y= *(double *)ptr;
- return g(x, y);
+	double y= *(double *)ptr;
+	return g(x, y);
 }
 
 double integral_g_dx(double xi, double xf, double y, int n) {
- return integral(g_aux, &y, xi, xf, n);
+	return integral(g_aux, &y, xi, xf, n);
 }
+
+typedef struct {
+	double xi, xf;
+	int n;
+} Param;
+
+
+double integral_g_dx_aux(void *p, double y){
+	double xi = ((Param *)p)->xi;
+	double xf = ((Param *)p)->xf;
+	int n = ((Param *)p)->n;
+	return integral_g_dx(xi,xf,y,n);
+}
+
+double integral_g_dx_dy_par(double xi, double xf, int n, double yi, double yf, int m){
+	Param* p = (Param *) malloc(sizeof(Param));
+	p->xi=xi;
+	p->xf=xf;
+	p->n=n;
+	return integral_g_dx_aux(&p,);
+}
+
+double integral_g_dx_dy(double xi, double xf, int n, double yi, double yf, int m){
+	Param* p = (Param *) malloc(sizeof(Param));
+	p->xi=xi;
+	p->xf=xf;
+	p->n=n;
+	return integral(integral_g_dx_dy_par, &p, yi, yf, m);
+}
+
+
 
 /* Programe aca la funcion integral_g_dx_dy.
  *
@@ -26,16 +57,12 @@ double integral_g_dx(double xi, double xf, double y, int n) {
  *
  * Tambien debe invocar integral en la funcion integral_g_dx_dy:
  *
- * double integral_g_dx_aux( ... ) {
- *   ... integral(integral_g_dx_aux, ...);
+ * double integral_g_dx_aux( ... ) { //deberia ser _par
+ *   ... integral(integral_g_dx_aux, ...); 
  * }
  *
  * El truco esta en pasar en ptr una estructura Param con los parametros
  * xi, xf y n que se necesitan en integral_g_dx_aux.
  */
 
-typedef struct {
-  double xi, xf;
-  int n;
-} Param;
 
