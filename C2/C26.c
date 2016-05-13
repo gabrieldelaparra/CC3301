cd9c3c523ebe159226f2c610bbab0ad2a17188a0
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2016-05-12 22:36:17 -0300

    Changes C26

--- C2/C26.c
+++ C2/C26.c
@@ -12,6 +12,7 @@ typedef struct {
 
 void Stack_Init(Stack *S)
 {
+	S = (Stack *)malloc(sizeof(S));
     S->size = 0;
 }
 
@@ -47,7 +48,7 @@ void Stack_Pop(Stack *S)
 
 typedef char Ingredientes;
 typedef char Pizza;
-typedef char Cola;
+typedef Stack *Cola;
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -61,39 +62,58 @@ typedef struct {
 Stack *cola;
 pthread_t operador;
 
+
+
 Orden *ordenar(Ingredientes *ingr){
 	Orden *o = malloc(sizeof(*o));
 	o->ingr = ingr;
 	o->fin = 0;
+	printf("agregar a cola\n");
+	pthread_mutex_lock(&m);
 	Stack_Push(cola,(void*)o);
+	pthread_cond_broadcast(&cond);
+	pthread_mutex_unlock(&m);
 	return o;
 }
 
-Pizza *pizzaCruda(Ingredientes *ingr){
+void *ord(void *p){
+	ordenar((Ingredientes *)p);
 	return NULL;
 }
 
+Pizza *pizzaCruda(Ingredientes *ingr){
+	return (Pizza*)ingr;
+}
+
 void hornear(Pizza *pizzas[]){
-	// return NULL;
+	printf("Hornear pizzas");
 }
 
 void *pizzeria(void *p){
+	pthread_mutex_lock(&m);
 	for(;;){
 		Pizza *pizzas[4];
 		Orden *ords[4];
+		printf("Cola: %d\n", Stack_Empty(cola));
 		while(Stack_Empty(cola))
-			;
+			printf("Cola: %d\n", Stack_Empty(cola));
+			pthread_cond_wait(&cond, &m);
 		int i = 0, j;
 		while(!Stack_Empty(cola) && i<4){
 			ords[i] = (Orden *)Stack_Top(cola);
 			pizzas[i] = pizzaCruda(ords[i]->ingr);
 			i++;
 		}
+		pthread_mutex_unlock(&m);
+		
 		hornear(pizzas); //lento
+		
+		pthread_mutex_lock(&m);
 		for(j = 0;j<i;j++){
 			ords[i]->fin=1;
 			ords[i]->pizza = pizzas[i];
 		}
+		pthread_cond_broadcast(&cond);
 	}
 }
 
@@ -106,10 +126,19 @@ Pizza *retirar(Orden *o){
 }
 
 void initHorno(){
-	// cola = nuevaC
+	Stack_Init(cola);
 	pthread_create(&operador, NULL, pizzeria, NULL);
 }
 
 int main(int argc, char *argv[]){
 	initHorno();
+pthread_t t1;
+pthread_t t2;
+// pthread_t t3;
+// pthread_t t4;
+pthread_create(&t1,NULL,ord,(void *)'i');
+pthread_create(&t2,NULL,ord,(void *)'a');
+	
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
 }
